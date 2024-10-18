cea089906ff3c6416393518fc67e695e7a4e2d5e
Andrii-Medvediev <andrmedvediev@gmail.com>
2024-10-18 20:24:45 +0200

    first commit

---
+++ kitchen/Cook.java
@@ -0,0 +1,53 @@
+package kitchen;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Cook implements Runnable {
+
+    private static final int NUM_COOKS = 4; // Кількість кухарів
+    private static final AtomicInteger cookCounter = new AtomicInteger(0); // Лічильник для черговості кухарів
+    private int cookId; // Ідентифікатор кухаря
+
+    @Override
+    public void run() {
+        try {
+            String orderName = Thread.currentThread().getName(); // Ідентифікатор замовлення
+
+            System.out.printf("Розміщення %s.\n", orderName);
+            Thread.sleep(1000); // Імітація розмішення замовлення
+            Main.newOrder();
+
+            // Безкінечний цикл очікування на вільного кухаря
+            while (true) {
+                // Перевіряємо, чи відкрита кухня. Якщо кухня закрита, завершуємо обробку замовлення
+                if (!Main.isKitchenOpen()) {
+                    System.out.printf("Кухня закрита. Ресторан не може приготувати %s.\n", orderName);
+                    Main.completeOrder();
+                    return;
+                }
+
+                // Чекаємо на доступного кухаря (семафор гарантує, що одночасно працює не більше 4 кухарів)
+                if (Main.cooks.tryAcquire()) {
+                    // Призначаємо кухаря по черговості
+                    cookId = cookCounter.getAndIncrement() % NUM_COOKS + 1;
+
+                    System.out.printf("Кухар %d прийняв %s та розпочинає приготування.\n", cookId, orderName);
+                    Thread.sleep(2000); // Імітація приготування страви
+
+                    System.out.printf("Кухар %d завершив %s.\n", cookId, orderName);
+                    Thread.sleep(20);
+
+                    Main.cooks.release();
+                    break;
+                } else {
+                    Thread.sleep(100);  // Затримка перед повторною спробою
+                }
+            }
+
+            Main.completeOrder();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ kitchen/Main.java
@@ -0,0 +1,61 @@
+package kitchen;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    static final Semaphore cooks = new Semaphore(4);
+    private static boolean isOpen = true;
+    private static final AtomicInteger ordersInProgress = new AtomicInteger(0);
+
+    public static synchronized boolean isKitchenOpen() {
+        return isOpen;
+    }
+
+    public static synchronized void closeKitchen() {
+        isOpen = false;
+        System.err.println("===================== Кухня закрилася =====================");
+    }
+
+    public static synchronized void newOrder() {
+        ordersInProgress.incrementAndGet();
+    }
+
+    public static synchronized void completeOrder() {
+        ordersInProgress.decrementAndGet();
+    }
+
+    public static synchronized boolean areOrdersPresent() {
+        return ordersInProgress.get() > 0;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.err.println("==================== Кухня  відкрилася ====================");
+
+        Runnable kitchen = () -> {
+            int i = 0;
+            while (isKitchenOpen()) {
+                new Thread(new Cook(), "Замовлення " + ++i).start();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread kitchenThread = new Thread(kitchen, "Кухня");
+        kitchenThread.start();
+
+        Thread.sleep(10_000);
+        closeKitchen();
+
+        while (areOrdersPresent()) {
+            Thread.sleep(1000);
+        }
+
+        System.err.println("==================== Всі кухарі вільні ====================");
+    }
+}
